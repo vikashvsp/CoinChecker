88ae34c7196b38b6e30a348b88ded3f1a28c6dbd
vikash kumar <vkvikashkumar987@gmail.com>
2023-09-10 10:12:13 +0530

    useState and useEffect defined

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import axios from "axios"
+import { useEffect, useState } from 'react';
 export default function App() {
+  const [myData, setMyData] = useState()
+  const apiUrl = "https://api.coincap.io/v2/assets/bitcoin"
+  const time = new Date()
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then(function (response) {
+        const data = response.data
+        setMyData(data.data)
+      })
+  }, [])
+  console.log(myData);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        myData && (
+          <>
+            <Text>{myData.id}</Text>
+            <Text>{myData.priceUsd}</Text>
+          </>
+        )
+      }
       <StatusBar style="auto" />
     </View>
   );
