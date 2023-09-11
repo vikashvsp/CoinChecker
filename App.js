05531eefc5b901aae6a20f750ab724f8610b3aee
vikash kumar <vkvikashkumar987@gmail.com>
2023-09-11 23:19:39 +0530

    useQuery setup done

--- App.js
+++ App.js
@@ -7,20 +7,25 @@ import Menu from './src/menu';
 import Main from './src/main';
 import { NavigationContainer } from '@react-navigation/native'; // Add this import
 import { createStackNavigator } from '@react-navigation/stack'; // Import createStackNavigator
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App() {
   const Stack = createStackNavigator(); // Create a stack navigator
+  const queryClient = new QueryClient();
 
   return (
     <SafeAreaView style={{ flex: 1, marginTop: '10%' }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="splash" component={Splash} options={{ headerShown: false }} />
-          <Stack.Screen name="menu" component={Menu} options={{ headerShown: false }} />
-          <Stack.Screen name="main" component={Main} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="splash" component={Splash} options={{ headerShown: false }} />
+            <Stack.Screen name="menu" component={Menu} options={{ headerShown: false }} />
+            <Stack.Screen name="main" component={Main} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
     </SafeAreaView>
+
   )
 }
 
--- src/menu/index.js
+++ src/menu/index.js
@@ -1,15 +1,33 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-
+import { useQuery, } from '@tanstack/react-query'
 const Menu = () => {
      const navigation = useNavigation()
+     const fetchCoinData = async () => {
+          const apiUrl = 'https://api.coincap.io/v2/assets/bitcoin';
+          const response = await fetch(apiUrl);
+          return response.json();
+     };
+     const { data, error, isLoading, refetch } = useQuery(['coinData'], fetchCoinData,
+          { refetchInterval: 4000, }
+     );
+
+     if (isLoading) {
+          console.log(isLoading)
+          return <Text>Loading...</Text>;
+     }
+
+     if (error) {
+          console.log(error);
+          return <Text>Error: {error.message}</Text>;
+     }
+     console.log("jo h wahi", data.data.priceUsd);
      return (
           <View>
                <TouchableOpacity onPress={() => {
                     navigation.navigate("splash")
                }}>
-
                     <Text>Menu</Text>
                </TouchableOpacity>
                <TouchableOpacity onPress={() => {
@@ -19,6 +37,12 @@ const Menu = () => {
                          Main
                     </Text>
                </TouchableOpacity>
+               {
+                    data &&
+                    <Text>
+                         {data.data.priceUsd}
+                    </Text>
+               }
           </View>
      )
 }
